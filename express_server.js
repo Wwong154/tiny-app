03f4d57df0b23b6290a8deab11cf238b936e5e9f
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-12-08 21:39:16 +0000

    added login and out cookies and function

--- express_server.js
+++ express_server.js
@@ -3,19 +3,20 @@ const {inspect} = require('util');
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 function generateRandomString() {
   return Math.random().toString(36).substring(4,10);
 }
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,12 +27,13 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+    const templateVars = { username: req.cookies["username"], urls: urlDatabase };
+    res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -40,11 +42,24 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect(`http://localhost:8080/urls/`);
 });
 
+app.post("/login", (req, res) => {
+  console.log(`New log in: ${req.body.username}`);  // Log the POST request body to the console
+  res.cookie("username",req.body.username);
+  res.redirect(`http://localhost:8080/urls/`);
+});
+
+app.post("/logout", (req, res) => {
+  console.log(`User has logged out`);  // Log the POST request body to the console
+  res.clearCookie("username");
+  res.redirect(`http://localhost:8080/urls/`);
+});
+
 app.post("/urls/:shortURL/update", (req, res) => {
   console.log(`Update: ${req.params.shortURL} to link to ${req.body.longURL}`);  // Log the POST request body to the console
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect(`http://localhost:8080/urls/`);
 });
+
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
@@ -60,7 +75,7 @@ app.get("/urls/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL]){
   res.redirect(`http://localhost:8080/urls/new`)
   } else {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
   }
 });
