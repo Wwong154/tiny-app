4492d647fc53b325072dbc97acf90f98d1afd6ee
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-12-09 23:36:28 +0000

    styling and linting, hopefully didn't break anything

--- express_server.js
+++ express_server.js
@@ -5,37 +5,37 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 
-function generateRandomString() {
+const generateRandomString = function() {
   return Math.random().toString(36).substring(4,10);
-}
+};
 
-function checkUserID(id) {
+const checkUserID = function(id) {
   if (users[id]) {
     return users[id].email;
   }
   return false;
-}
+};
 
-function checkUserExist(email) {
+const checkUserExist = function(email) {
   const keys = Object.keys(users);
-  for(const user of keys) {
-    if (users[user].email === email){
+  for (const user of keys) {
+    if (users[user].email === email) {
       return users[user].id;
     }
   }
   return false;
-}
+};
 
-function urlsForUser(id) {
+const urlsForUser = function(id) {
   const shortURL = Object.keys(urlDatabase);
   let result = [];
-  for(const url of shortURL) {
-    if (urlDatabase[url].userID === id){
+  for (const url of shortURL) {
+    if (urlDatabase[url].userID === id) {
       result.push(url);
     }
   }
   return result;
-}
+};
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -47,23 +47,23 @@ const urlDatabase = {
 };
 
 
-const users = { 
+const users = {
   "ID1": {
-    id: "ID1", 
-    email: "user@example.com", 
+    id: "ID1",
+    email: "user@example.com",
     password: "1234"
   },
- "ID2": {
-    id: "ID2", 
-    email: "user2@example.com", 
+  "ID2": {
+    id: "ID2",
+    email: "user2@example.com",
     password: "abcd"
   },
   "ID3": {
-    id: "ID3", 
-    email: "user3@example.com", 
+    id: "ID3",
+    email: "user3@example.com",
     password: "ABCD"
   }
-}
+};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -76,21 +76,21 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   if (req.cookies["user_id"]) {
     let keys = urlsForUser(req.cookies["user_id"]);
-    const templateVars = { user_email: checkUserID(req.cookies["user_id"]), urls: urlDatabase, keys: keys, res: '', err: ''};
+    const templateVars = { userEmail: checkUserID(req.cookies["user_id"]), urls: urlDatabase, keys: keys, res: '', err: ''};
     if (!keys.length) {
       templateVars.keys = false;
-    };
+    }
     res.render("urls_index", templateVars);
   } else {
     res.status(403);
-    const templateVars = { user_email: checkUserID(req.cookies["user_id"]), urls: urlDatabase, keys: '', res: 403, err: 'Please login first!'};
+    const templateVars = { userEmail: checkUserID(req.cookies["user_id"]), urls: urlDatabase, keys: '', res: 403, err: 'Please login first!'};
     res.render("urls_index", templateVars);
   }
 });
 
 app.get("/urls/new", (req, res) => {
   if (req.cookies["user_id"]) {
-    const templateVars = { user_email: checkUserID(req.cookies["user_id"]) };
+    const templateVars = { userEmail: checkUserID(req.cookies["user_id"]) };
     res.render("urls_new", templateVars);
   } else {
     res.redirect('/login');
@@ -100,25 +100,25 @@ app.get("/urls/new", (req, res) => {
 app.post("/register", (req, res) => {
   if (checkUserExist(req.body.email)) {
     res.status(400);
-    const templateVars = { user_email: checkUserID(req.cookies["user_id"]), res: 400, err: "Email already registered, please use login instead"};
+    const templateVars = { userEmail: checkUserID(req.cookies["user_id"]), res: 400, err: "Email already registered, please use login instead"};
     res.render("register", templateVars);
-  } else if (req.body.email && req.body.password){
-    const id = generateRandomString()
+  } else if (req.body.email && req.body.password) {
+    let id = generateRandomString();
     while (users[id]) {
       id = generateRandomString();
     }
     users[id] = {id: id, email: req.body.email, password: req.body.password};
-    res.cookie("user_id",id)
+    res.cookie("user_id",id);
     res.redirect(`/urls/`);
   } else {
     res.status(400);
-    const templateVars = { user_email: checkUserID(req.cookies["user_id"]), res: 400, err: "Please enter BOTH email or password"};
+    const templateVars = { userEmail: checkUserID(req.cookies["user_id"]), res: 400, err: "Please enter BOTH email or password"};
     res.render("register", templateVars);
   }
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = { user_email: checkUserID(req.cookies["user_id"]), res: '', err: ''};
+  const templateVars = { userEmail: checkUserID(req.cookies["user_id"]), res: '', err: ''};
   res.render("register", templateVars);
 });
 
@@ -129,28 +129,28 @@ app.post("/urls/:shortURL/delete", (req, res) => {
     res.redirect(`/urls/`);
   } else {
     res.status(403);
-    res.send("you are not the owner of this url")
+    res.send("you are not the owner of this url");
   }
 });
 
 app.get("/login", (req, res) => {
-  const templateVars = { user_email: checkUserID(req.cookies["user_id"]),res: '', err: ''};
+  const templateVars = { userEmail: checkUserID(req.cookies["user_id"]),res: '', err: ''}; //get is always first try, so no error
   res.render("login", templateVars);
 });
 
 app.post("/login", (req, res) => {
-  const userid = checkUserExist(req.body.email)
-  if (userid && users[userid].password === req.body.password) {
+  const userid = checkUserExist(req.body.email);
+  if (userid && users[userid].password === req.body.password) {// if password matched, log the user in
     console.log(`New log in: ${userid}`);  // Log the POST request body to the console
     res.cookie("user_id",checkUserExist(req.body.email));
     res.redirect(`/urls/`);
-  } else if (userid) {
+  } else if (userid) {// if password do not matched, shown error of not matching
     res.status(403);
-    const templateVars = { user_email: checkUserID(req.cookies["user_id"]), res: 403, err: "The email and password combination does not match our record"};
+    const templateVars = { userEmail: checkUserID(req.cookies["user_id"]), res: 403, err: "The email and password combination does not match our record"};
     res.render("login", templateVars);
-  } else {
+  } else {// if email doesn't exist, tell user
     res.status(403);
-    const templateVars = { user_email: checkUserID(req.cookies["user_id"]), res: 403, err: "The email you entered is not valid"};
+    const templateVars = { userEmail: checkUserID(req.cookies["user_id"]), res: 403, err: "The email you entered is not valid"};
     res.render("login", templateVars);
   }
 });
@@ -168,7 +168,7 @@ app.post("/urls/:shortURL/update", (req, res) => {
     res.redirect(`/urls/`);
   } else {
     res.status(403);
-    res.send("you are not the owner of this url")
+    res.send("you are not the owner of this url");
   }
 });
 
@@ -179,28 +179,28 @@ app.post("/urls", (req, res) => {
     shorten = generateRandomString();
   }
   urlDatabase[shorten] = { longURL: req.body.longURL, userID: req.cookies.user_id };
-  console.log(urlDatabase[shorten].userID)
+  console.log(urlDatabase[shorten].userID);
   res.redirect(`/urls/${shorten}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   if (req.cookies["user_id"] === urlDatabase[req.params.shortURL].userID) {
-    if (!urlDatabase[req.params.shortURL]){
-      res.redirect(`/urls/new`)
+    if (!urlDatabase[req.params.shortURL]) {
+      res.redirect(`/urls/new`);
     } else {
-      const templateVars = { user_email: checkUserID(req.cookies["user_id"]), shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, res: '', err: '' };
+      const templateVars = { userEmail: checkUserID(req.cookies["user_id"]), shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, res: '', err: '' };
       res.render("urls_show", templateVars);
     }
   } else {
-    res.status(403)
-    const templateVars = { user_email: checkUserID(req.cookies["user_id"]), shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, res: 403, err: 'Not owner of url' };
+    res.status(403);
+    const templateVars = { userEmail: checkUserID(req.cookies["user_id"]), shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, res: 403, err: 'Not owner of url' };
     res.render("urls_show", templateVars);
   }
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  if (urlDatabase[req.params.shortURL] === []){
-    res.redirect(`/urls/new`)
+  if (urlDatabase[req.params.shortURL] === []) {
+    res.redirect(`/urls/new`);
   } else {
     let longurl = urlDatabase[req.params.shortURL].longURL;
     res.redirect(longurl);
